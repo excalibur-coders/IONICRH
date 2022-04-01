6241103aa9fa8e4fd0b47e893d74dd3c46e5e6df
Priscila  Silva <prialmeida1801@gmail.com>
2022-04-01 17:27:37 -0300

    estilização do navbar

--- ionic_rh_front/src/components/navbar/styles.ts
+++ ionic_rh_front/src/components/navbar/styles.ts
@@ -1,5 +1,31 @@
 import styled from '@emotion/styled';
+import { Flex } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
+import { theme } from 'theme';
 
 export const Container = styled.div`
 
+img {
+  width: 120px;
+  margin-top: 10px;
+  margin: 5px 5px;
+
+}
+
+`;
+
+export const ContainerButton = styled.div`
+
+.button {
+  width: 70px;
+  margin-top: 10px;
+  margin: 5px 5px;
+  border-radius: 200px;
+  opacity: 1 ;
+
+}
+
+`;
+export const ContainerFlex = styled(Flex)`
+  justify-content: space-between;
 `;
--- ionic_rh_front/src/routes/index.tsx
+++ ionic_rh_front/src/routes/index.tsx
@@ -1,14 +1,13 @@
 import Login from 'pages/Login';
-<<<<<<< HEAD
-import Funcionarios from 'pages/Login/funcionários';
-=======
+///<<<<<<< HEAD
 import CriarConta from 'pages/CriarConta';
 import ContaSucesso from 'pages/ContaSucesso';
 import RecuperarSenha from 'pages/RecuperarSenha';
 import ResetarSenha from 'pages/ResetarSenha';
 import SenhaAlterada from 'pages/SenhaAlterada';
+import Funcionarios from 'pages/funcionários';
 
->>>>>>> 3bdfe9d09531ee5d4c840242128d3ff745e9a39c
+//>>>>>>> 3bdfe9d09531ee5d4c840242128d3ff745e9a39c
 import { Route, BrowserRouter, Routes as RoutesWrapper } from 'react-router-dom';
 
 function Routes() {
@@ -16,15 +15,12 @@ function Routes() {
     <BrowserRouter>
       <RoutesWrapper>
         <Route path="/" element={<Login />} />
-<<<<<<< HEAD
         <Route path="/Funcionarios" element={<Funcionarios />} />
-=======
         <Route path="/Cadastro" element={<CriarConta />} />
         <Route path="/ContaSucesso" element={<ContaSucesso />} />
         <Route path="/RecuperarSenha" element={<RecuperarSenha />} />
         <Route path="/Resetar" element={<ResetarSenha />} />
         <Route path="/SenhaAlterada" element={<SenhaAlterada />} />
->>>>>>> 3bdfe9d09531ee5d4c840242128d3ff745e9a39c
       </RoutesWrapper>
     </BrowserRouter>
   )
