ca046827a1106687c6a7c63ea2627e5fe804cd22
ZeroPirata <gabriel0gsbn@gmail.com>
2022-04-05 23:17:47 -0300

    Acesso [Update, Get, Delete, Post] criado

---
+++ ionic_rh_back/controllers/Acesso.ts
@@ -0,0 +1,71 @@
+import { AppDataSource } from "../config/database";
+import { Acesso } from "../models/acesso_sistema";
+
+const acesso_sistemaRepository = AppDataSource.getRepository(Acesso);
+
+export const getAllAcesso = async (req, res) => {
+    try {
+      const acesso = await acesso_sistemaRepository.find({
+        select: {
+          acesso_cargo: true,
+        }
+      });
+      res.json(acesso);
+    } catch (error) {
+      res.json(error);
+    }
+  }
+export const getAcessoById = async (req, res) => {
+    try {
+        const acesso = await acesso_sistemaRepository.findOne({
+            where: {
+                acesso_id: req.params.id
+            }
+        });
+        res.json(acesso);
+    } catch (error) {
+        res.json(error);
+    }
+}
+export const createAcesso = async (req, res) => {
+    try {
+        const acesso_sistema = req.body;
+        const acesso_cargo = acesso_sistema.acesso_cargo;
+        const acesso = await acesso_sistemaRepository.save(req.body);
+        res.json({
+            "Message": `O Acesso ${acesso_cargo} foi criado`
+        });
+    } catch (error) {
+        res.json(error);
+    }
+}
+export const updateAcesso = async (req, res) => {
+    try {
+        await acesso_sistemaRepository
+            .createQueryBuilder()
+            .update(Acesso)
+            .set({
+                "acesso_cargo": req.body.acesso_cargo
+            }).where("acesso_id = :acesso_id", { acesso_id: req.params.id }).execute();
+        res.json({
+            "message": `O Acesso foi atualizado com sucesso`
+        });
+    } catch (error) {
+        res.json(error);
+    }
+}
+export const deleteAcesso = async(req, res) => {
+    try {
+        await acesso_sistemaRepository
+        .createQueryBuilder()
+        .delete()
+        .from(Acesso)
+        .where("acesso_id = :acesso_id",{acesso_id:req.params.id})
+        .execute()
+        res.json({
+            "message":"Acesso deletado com sucesso"
+        })
+    } catch (error) {
+        
+    }
+}
