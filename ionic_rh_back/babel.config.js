d2400f17d8b99a99182d5622229996a83f67c3e4
Lucas Braz <lucasbrzdias@gmail.com>
2022-04-06 10:59:39 -0300

    feat: 🎸 babebl congi refactor

--- ionic_rh_back/babel.config.js
+++ ionic_rh_back/babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
+    [
+      '@babel/preset-env', 
+      { targets: { node: 'current' } }
+    ],
     '@babel/preset-typescript',
   ],
   plugins: [
@@ -8,9 +11,16 @@ module.exports = {
       'module-resolver',
       {
         alias: "./src",
-      }],
-      'babel-plugin-transform-typescript-metadata',
-      ["@babel/plugin-proposal-decorators", { "legacy": true }],
-      ["@babel/plugin-proposal-class-properties", { "loose": true }]
+      }
+    ],
+    'babel-plugin-transform-typescript-metadata',
+    [
+      "@babel/plugin-proposal-decorators", 
+      { "legacy": true }
+    ],
+    [
+      "@babel/plugin-proposal-class-properties", 
+      { "loose": true }
+    ]
   ]
 }
