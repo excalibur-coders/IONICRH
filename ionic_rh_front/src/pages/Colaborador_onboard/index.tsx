0a6594dd08c03e76c8908e523a6e85d86bb28f0d
KevinFMfatec <mirendakevin@gmail.com>
2022-04-07 08:06:51 -0300

    colaborador_pages

--- ionic_rh_front/src/pages/Colaborador_onboard/index.tsx
+++ ionic_rh_front/src/pages/Colaborador_onboard/index.tsx
@@ -1,6 +1,10 @@
 import Colab_Nav from 'components/Colab_Nav';
 import { ReactNode } from 'react';
 import { theme } from 'theme';
+import { BsClipboardCheck } from "react-icons/bs";
+
+import * as S from './styles';
+import { Button } from '@chakra-ui/react';
 //import * as S from './styles';
 
 
@@ -8,6 +12,24 @@ function Colab_onboard(){
   return (
     <>
     <Colab_Nav/>
+    <S.Container>
+      <h1>Cursos</h1>
+      <div className='buttons_onboard'>
+        <div>
+          <Button className='button' as={BsClipboardCheck} color={ theme.colors.font}></Button>
+          <small>Curso RDC-16</small>
+          </div>
+          <div>
+          <Button className='button' as={BsClipboardCheck} color={ theme.colors.font}></Button>
+          <small>Curso 2</small>
+          </div>
+          <div>
+          <Button className='button' as={BsClipboardCheck} color={ theme.colors.font}></Button>
+          <small>Curso 3</small>
+          </div>
+      </div>
+      <footer>IonicRH - Documentação e política de privacidade</footer>
+    </S.Container>
     </>
   )
 }
--- ionic_rh_front/src/pages/Colaborador_onboard/styles.ts
+++ ionic_rh_front/src/pages/Colaborador_onboard/styles.ts
@@ -1,6 +1,44 @@
-import { ReactNode } from 'react';
 import { theme } from 'theme';
 import {Flex} from '@chakra-ui/react';
 import styled from '@emotion/styled'
 
-export const Container = styled(Flex)``;
+export const Container = styled(Flex)`
+display: flex;
+flex-direction: column;
+align-items: center;
+
+h1 {
+    margin-top: 2rem;
+    margin: auto;
+    text-align: center;
+    font-weight: bold;
+    font-size: 70px;
+    height: 100px;
+}
+.buttons_onboard {
+    margin-top: 5rem;
+    display: flex;
+
+    small {
+      text-align: center;
+      font-weight: bold;
+      font-size: 20px;
+      display: block; 
+      text-align: center;
+    }
+
+    .button {
+      height: 150px;
+      margin: 50px 50px;
+    }
+}
+
+footer {
+  margin-top: 13rem;
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px;
+  text-align: center;
+  
+}
+`;
