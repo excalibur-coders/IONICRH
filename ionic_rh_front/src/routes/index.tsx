b4517af3ae69720358924720c5f203e805021f14
taissalomao <tais.salomao@fatec.sp.gov.br>
2022-04-07 10:28:46 -0300

    merge

--- ionic_rh_front/src/routes/index.tsx
+++ ionic_rh_front/src/routes/index.tsx
@@ -1,7 +1,5 @@
 import Login from 'pages/Login';
-<<<<<<< HEAD
 import PerfilAdmin from 'pages/Perfil_Admin';
-=======
 import CriarConta from 'pages/CriarConta';
 import ContaSucesso from 'pages/ContaSucesso';
 import RecuperarSenha from 'pages/RecuperarSenha';
@@ -9,7 +7,6 @@ import ResetarSenha from 'pages/ResetarSenha';
 import SenhaAlterada from 'pages/SenhaAlterada';
 import Funcionarios from 'pages/funcionários';
 
->>>>>>> b4d40b6e4a1bf294c7fb2f6b926226ef4bb66db8
 import { Route, BrowserRouter, Routes as RoutesWrapper } from 'react-router-dom';
 
 function Routes() {
