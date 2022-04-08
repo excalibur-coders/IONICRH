a991471e420731774f2868036f97b320f8d78082
KevinFMfatec <mirendakevin@gmail.com>
2022-04-08 07:33:55 -0300

    dados do usuario

--- ionic_rh_front/src/pages/Colaborador_user/index.tsx
+++ ionic_rh_front/src/pages/Colaborador_user/index.tsx
@@ -14,13 +14,41 @@ function Colab_user(){
     <>
     <Colab_Nav/>
     <S.Container>
-        <div className='leftWrapper'>
-            <div className='foto'>
-                <MdAccountCircle  size='100%'/>
-            </div>
-        </div>
-        <div className='rightWrapper'>
+        <div className='Wrapper'>
+            <div className='centerWrapper'>  
+                <div className='leftWrapper'>
+                    <div className='foto'>
+                        <MdAccountCircle  size='100%'/>
+                    </div>
 
+                    <div className='User'>
+                        <h1>User</h1>
+                    </div>
+
+                    <div className='coluna'>
+                        <div className='coluna1'>
+                            <span>Cargo:</span> 
+                            <span>RG:</span> 
+                            <span>CPF:</span> 
+                            <span>Matrícula:</span> 
+                            <span>Departamento:</span> 
+                        </div>
+                    </div>
+
+                    <div className='coluna'>
+                        <div className='coluna2'>
+                            <span>Rua/Av:</span> 
+                            <span>N:</span> 
+                            <span>Compl:</span> 
+                            <span>Matrícula:</span> 
+                            <span>Departamento:</span> 
+                        </div>
+                    </div>
+                </div>
+                <div className='rightWrapper'>
+
+                </div>
+            </div>
         </div>
     </S.Container>
     </>
--- ionic_rh_front/src/pages/Colaborador_user/styles.ts
+++ ionic_rh_front/src/pages/Colaborador_user/styles.ts
@@ -3,30 +3,73 @@ import {Flex} from '@chakra-ui/react';
 import styled from '@emotion/styled'
 
 export const Container = styled(Flex)`
+height:100vh;
 display: flex;
+flex-direction:column;
+color: ${theme.colors.font};
+
+Wrapper{
+    position:relative;
+    display: flex;
+    justify-content: center;
+    text-align: justify;
+}
+
+.centerWrapper{
+    margin: 0 auto;
+    display: flex;
+    flex-wap: wap;
+}
 
 .leftWrapper{
-border: 1px solid red;
-width: 20%;
-height: 95vh;
-display: flex;
-flex-direction: column;
-align-items: center;
+    position: relative;
+    float: left;
+    width: 30%;
+    margin-top: 50px;
+    text-align: center;
 
  .foto{
-     margin-top: 50px;
      border: 1px solid black;
      border-radius: 90px;
      width: 150px;
      height: 150px;
+     margin: 0 auto;
      box-shadow: 0 0 1em black;
  }
+
+ .User{
+     margin: 0 auto;
+     margin-top:20px;
+ }
+
+ .User h1 {
+     text-align:center;
+     font-weight: bold;
+     font-size: 30px;
+     margin-bottom: 20px;
+ }
+ 
+ .coluna{
+     width:100%;
+ }
+
+ .coluna1{
+     display:flex;
+     flex-direction: column;
+     text-align:left;
+     padding: 0 50px;
+ }
+
+.coluna1 span{
+    font-weight: bold;
+    font-size: 20px;
+}
 
 }
 
 .rightWrapper{
     border: 1px solid blue;
-    width: 80%;
+    width: 70%;
     display: flex;
     flex-direction: column;
     align-items: center;
