570c5ca0cce8729ee982f99864b9eb0c27402a71
KevinFMfatec <mirendakevin@gmail.com>
2022-04-08 10:35:44 -0300

    Criação da tela do "Usuário"

--- ionic_rh_front/src/pages/Colaborador_user/index.tsx
+++ ionic_rh_front/src/pages/Colaborador_user/index.tsx
@@ -3,6 +3,7 @@ import { MdAccountCircle } from 'react-icons/md';
 import { ReactNode } from 'react';
 import { theme } from 'theme';
 import { BsClipboardCheck } from "react-icons/bs";
+import { MdArrowForward } from "react-icons/md";
 
 import * as S from './styles';
 import { Button } from '@chakra-ui/react';
@@ -37,6 +38,7 @@ function Colab_user(){
 
                     <div className='coluna'>
                         <div className='coluna2'>
+                            <h2>Endereço</h2>
                             <span>Rua/Av:</span> 
                             <span>N:</span> 
                             <span>Compl:</span> 
@@ -46,10 +48,45 @@ function Colab_user(){
                     </div>
                 </div>
                 <div className='rightWrapper'>
-
+                    <div className='linhaDados'>
+                        <div className='dadosUser1'>
+                            <span>E-mail:</span> 
+                            <span>Data de nascimento:</span> 
+                            <span>Telefone:</span> 
+                            <span>Tipo de contrato:</span> 
+                        </div>    
+                        <div className='dadosUser2'>
+                            <span>Estado civil:</span>
+                            <span>Sexo:</span>
+                            <span>Escolaridade:</span>
+                            <span>Turno:</span>
+                        </div>
+                    </div>
+                    <div className='cargo'>
+                        <h3><b>Descrição do cargo:</b> Informações e tarefas a serem desempenhadas em uma função específica da empresa. Nessa descrição de atividades são registrados o objetivo, responsabilidades, escopo de trabalho, requisitos, condições de trabalhos e outros.</h3>
+                    </div>
+                    <h4>Cursos realizados:</h4>
+                    <div className='buttons_onboard'>
+                        <div>
+                        <Button className='button' as={BsClipboardCheck} color={ theme.colors.font}></Button>
+                        <small>Curso RDC-16</small>
+                        </div>
+                        <div>
+                        <Button className='button' as={BsClipboardCheck} color={ theme.colors.font}></Button>
+                        <small>Curso 2</small>
+                        </div>
+                        <div>
+                        <Button className='button' as={BsClipboardCheck} color={ theme.colors.font}></Button>
+                        <small>Curso 3</small>
+                        </div>
+                        <div>
+                        <Button className='button' as={MdArrowForward} color={ theme.colors.font}></Button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        <h5>IonicRH - Documentação e política de privacidade</h5>
     </S.Container>
     </>
   )
--- ionic_rh_front/src/pages/Colaborador_user/styles.ts
+++ ionic_rh_front/src/pages/Colaborador_user/styles.ts
@@ -24,8 +24,8 @@ Wrapper{
 .leftWrapper{
     position: relative;
     float: left;
-    width: 30%;
-    margin-top: 50px;
+    width: 25%;
+    margin-top: 40px;
     text-align: center;
 
  .foto{
@@ -65,14 +65,107 @@ Wrapper{
     font-size: 20px;
 }
 
+
+
+.coluna2{
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    padding: 0 50px;
+    
+    h2{
+        margin-top: 20px;
+        display: flex;
+        flex-direction: column;
+        text-align: left;
+        padding: 0 50px;
+        font-weight: bold;
+        font-size: 20px;
+    }
+
+    .coluna2 span{
+    font-weight: bold;
+    font-size: 20px;
+}
+}
+
 }
 
 .rightWrapper{
-    border: 1px solid blue;
-    width: 70%;
+    width: 75%;
     display: flex;
     flex-direction: column;
-    align-items: center;
 
+    .linhaDados{
+        display: flex;
+        flex-direction: row;
+
+        .dadosUser1{
+            margin-top: 40px;
+            display: flex;
+            flex-direction: column;
+            align-items: left;
+            width: 50%;
+            font-weight: bold;
+            font-size: 20px;
+            padding: 0 50px;
+        }
+        .dadosUser2{
+            margin-top: 40px;
+            display: flex;
+            flex-direction: column;
+            align-items: left;
+            width: 50%;
+            font-weight: bold;
+            font-size: 20px;
+            padding: 0 50px;
+        }
+    }
+
+    .cargo {
+        display: flex;
+        flex-direction: row;
+        padding: 40px 50px;
+
+        h3{
+            font-size: 20px;
+        }
+    }
+    h4 {
+        text-align: left;
+        font-weight: bold;
+        font-size: 20px;
+        padding: 40px 50px;
+    }
+    .buttons_onboard {
+        display: flex;
+    
+        small {
+          text-align: center;
+          font-weight: bold;
+          font-size: 20px;
+          display: block; 
+          text-align: center;
+        }
+    
+        .button {
+          height: 120px;
+          margin: 20px 50px;
+        }
+    }
+    footer {
+        margin-top: 13rem;
+        text-align: center;
+        font-weight: bold;
+        font-size: 20px;
+        text-align: center;
+    }
 }
+h5 {
+    margin-top: 5rem;
+    text-align: center;
+    font-weight: bold;
+    font-size: 20px;
+    text-align: center;
+  }
 `
