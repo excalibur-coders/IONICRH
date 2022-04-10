d6a6b994db0c46e58d293ed3950c348853f40046
ZeroPirata <gabriel0gsbn@gmail.com>
2022-04-10 16:41:42 -0300

    VerifyUserRole na rota GettAllUser

--- ionic_rh_back/src/routes/user.ts
+++ ionic_rh_back/src/routes/user.ts
@@ -7,7 +7,7 @@ import {
   updateUser,
   getLoggedUserData
 } from "controllers/User";
-import { auth } from 'middlewares/auth'
+import { auth, verifyUserRole } from 'middlewares/auth'
 import {
   verifyUserExistsByEmail,
   verifyLoginRequestValues, 
@@ -26,6 +26,6 @@ router.post('/login', verifyLoginRequestValues, loginUser)
 router.use(auth);
 router.put('/update', updateUser);
 router.get('/user-info', getLoggedUserData);
-router.get('/allUser',  getAllUser);
+router.get('/allUser', verifyUserRole(["Administrador"]),  getAllUser);
 
 export default router; 
