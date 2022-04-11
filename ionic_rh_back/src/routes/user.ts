99ee5019307344d661d78bcf6933c3412cd2a722
ZeroPirata <gabriel0gsbn@gmail.com>
2022-04-11 00:40:53 -0300

    Adicao das novas rotas de controle

--- ionic_rh_back/src/routes/user.ts
+++ ionic_rh_back/src/routes/user.ts
@@ -1,17 +1,27 @@
 import express from "express";
 
-import { 
-  CadastroUser, 
-  loginUser, 
-  getAllUser, 
+import {
+  CadastroUser,
+  loginUser,
+  getAllUser,
   updateUser,
-  getLoggedUserData
+  getLoggedUserData,
+  adicionarIdioma,
+  adicioanrEscolaridade,
+  getUserById
 } from "controllers/User";
-import { auth, verifyUserRole } from 'middlewares/auth'
+import {
+  insertContratoUser
+} from 'controllers/contrato'
+import {
+  auth,
+  verifyUserRole
+} from 'middlewares/auth'
+
 import {
   verifyUserExistsByEmail,
-  verifyLoginRequestValues, 
-  verifyRegisterRequestValues 
+  verifyLoginRequestValues,
+  verifyRegisterRequestValues
 } from "middlewares/user";
 
 const router = express.Router();
@@ -19,13 +29,23 @@ const router = express.Router();
 router.post(
   '/cadastro',
   verifyUserExistsByEmail,
-  verifyRegisterRequestValues, 
+  verifyRegisterRequestValues,
   CadastroUser
 );
 router.post('/login', verifyLoginRequestValues, loginUser)
 router.use(auth);
-router.put('/update', updateUser);
-router.get('/user-info', getLoggedUserData);
-router.get('/allUser', verifyUserRole(["Administrador"]),  getAllUser);
+router.put(
+  '/update',
+  adicioanrEscolaridade,
+  adicionarIdioma,
+  updateUser
+);
+router.get('/logged-user-info', getLoggedUserData);
+router.get('/allUser', verifyUserRole(["Administrador"]), getAllUser);
 
+// Rotas de Contrato
+// Pegar o Contrato, iserir e retornar os valores
+router.get('/user-info/:id', verifyUserRole(["Administrador","Gestor"]), getUserById )
+router.post('/Inserir-contrato-user/:id', verifyUserRole(["Administrador","Gestor"]), insertContratoUser , getUserById )
+router.get('/Contrato-user', verifyUserRole(["Administrador","Gestor"]), getLoggedUserData)
 export default router; 
