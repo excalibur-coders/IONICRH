56b56cbc1229ff133b446d72a1cbdf3a64a8d01f
ViniciusAndBar <vini.zeus2002@gmail.com>
2022-04-12 12:35:58 -0300

    Tela_Confirmação _Login_Integração

--- ionic_rh_front/src/pages/CriarConta/index.tsx
+++ ionic_rh_front/src/pages/CriarConta/index.tsx
@@ -1,4 +1,12 @@
 // import { Input, Stack } from '@chakra-ui/react';
+import { useCallback, useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+import { parseCookies } from "nookies";
+
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
+
 import { ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 import { Checkbox, CheckboxGroup } from '@chakra-ui/react'
@@ -11,9 +19,56 @@ import { theme } from 'theme';
 import * as S from './styles';
 import Button from 'components/Button';
 
+import { AuthContext } from "hooks/useAuth";
+import { api } from 'services/api';
+
 // console.log(theme.colors.primary);
 
+interface InputsProps {
+  nome: string;
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
 function CriarConta() {
+  const [errorMessage, setErrorMessage]=useState('')
+
+  const onSubmit = useCallback(async (data: InputsProps) => {
+    console.log(data)
+    if(data.password===data.passwordConfirmation){
+      await api.post<InputsProps>('/user/cadastro', {
+      user_nome: data.nome,
+      user_email: data.email,
+      password: data.password,
+    }).then(({data}) => {console.log(data);}).catch(error => {
+      console.log(error)
+    });}
+    else {
+      setErrorMessage('Senhas Divergentes')
+    }
+    
+
+
+  }, []);
+
+  const schema = yup.object({
+    nome: yup.string().required ('Nome Obrigatório !!'),
+    email: yup.string().required('E-mail obrigatório'),
+    password: yup.string().required('Senha obrigatória!'),
+    passwordConfirmation: yup.string().required('Confirmação de Senha obrigatória !!!'),
+  }).required();
+
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InputsProps>({
+    mode: 'onBlur',
+    resolver: yupResolver(schema),
+  });
+
   return (
     <S.Container>
       <div className='centerWrapper'>
@@ -21,21 +76,23 @@ function CriarConta() {
           <img src={IonicLogo} />
 
           <div className='form'>
-            <h1>Criar Conta</h1>
-            <Input size='sm' width="auto" fontSize={15} labelText="Nome" />
-            <Input size='sm' width="auto" fontSize={15} labelText="E-mail" />
-            <Input size='sm' width="auto" fontSize={15} labelText="Senha" />
-            <Input size='sm' width="auto" fontSize={15} labelText="Repetir a Senha" />
-            <div className="formFooter">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <h1>Criar Conta</h1>
+              <Input size='sm' width="auto" fontSize={15} labelText="Nome" type='text' error={errors.nome?.message} {...register('nome')}/>
+              <Input size='sm' width="auto" fontSize={15} labelText="E-mail" type='text' error={errors.email?.message} {...register('email')} />
+              <Input size='sm' width="auto" fontSize={15} labelText="Senha" type='password' error={errors.password?.message} {...register('password')} />
+              <Input size='sm' width="auto" fontSize={15} labelText="Repetir a Senha" type='password' error={errors.passwordConfirmation?.message} {...register('passwordConfirmation')} />
+              <div className="formFooter">
               <div className="formFooterLeft">
                 <Checkbox isInvalid>Eu li e concordo com os:</Checkbox>
                 <span>Termos e condições</span>
               </div>
               <div>
-                <Button text="Criar" color={theme.colors.primary} />
+                <Button text="Criar" color={theme.colors.primary} type='submit' />
               </div>
             </div>
-
+            </form>
+          
             <div className='registerWrapper'>
               <span>Já tem uma conta?</span>
               <span><Link to="/">Entre aqui</Link></span>
