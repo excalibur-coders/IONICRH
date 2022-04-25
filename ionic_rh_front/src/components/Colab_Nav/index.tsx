418922c859a18cb694a3ab61dff062c620840d5d
taissalomao <tais.salomao@fatec.sp.gov.br>
2022-04-25 00:25:07 -0300

    Ajustes de links, botões, integrações de página home e ajustes de rotas.

--- ionic_rh_front/src/components/Colab_Nav/index.tsx
+++ ionic_rh_front/src/components/Colab_Nav/index.tsx
@@ -4,8 +4,14 @@ import {Box, Flex, Heading, Spacer, Button, ListIcon} from '@chakra-ui/react';
 import {MdAccountCircle, MdOutlineLogout} from 'react-icons/md'
 import logo from 'assets/svg/ionicrh_logo_gray.svg';
 import { theme } from 'theme';
+import { useNavigate } from 'react-router-dom';
+import {AuthContext} from 'hooks/useAuth'
+import { useContext } from 'react';
 
 function Colab_Nav() {
+  const { signOut } = useContext(AuthContext)
+  const navigate = useNavigate();
+
   return (
     <div className='navbar'>
     <Box bg="#f4f4f5" width='100%' boxShadow=' 2px 2px 2px 1px rgba(0, 0, 0, 0.2)' position='fixed'>
@@ -19,8 +25,24 @@ function Colab_Nav() {
             <Heading  size='md'>
                   <div className='buttons_colab'>
                   <S.ContainerButton>
-                    <a href=''><Button className='button' as={MdOutlineLogout} color={ theme.colors.font}></Button></a>
-                    <a href='Colab_user'><Button className='button' href="Colab_user" as={MdAccountCircle} color={theme.colors.font}></Button></a>
+                    <Button
+                      className='button'
+                      as={MdOutlineLogout}
+                      color={ theme.colors.font}
+                      onClick={() => {
+                        signOut();
+                        navigate('/');
+                      }}
+                    />
+                    <Button
+                      className='button'
+                      href="Colab_user"
+                      as={MdAccountCircle}
+                      color={theme.colors.font}
+                      onClick={() => {
+                        navigate('/Colab_user');
+                      }}
+                    />
                   </S.ContainerButton>
                   </div>
             </Heading>
