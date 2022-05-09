dacf5393e9b6401714c3f7534b80ab8477825a3d
ViniciusAndBar <vini.zeus2002@gmail.com>
2022-05-09 16:42:43 -0300

    Organograma simples

--- ionic_rh_front/src/pages/Organograma/components/Chart/styles.ts
+++ ionic_rh_front/src/pages/Organograma/components/Chart/styles.ts
@@ -4,8 +4,6 @@ import { Flex, Box } from '@chakra-ui/react';
 import { theme } from 'theme';
 
 export const Container = styled(Flex)`
-  .principal {
-  }
   .fotosPerfil {
     max-width: 58px;
     img {
--- ionic_rh_front/src/pages/Organograma/styles.ts
+++ ionic_rh_front/src/pages/Organograma/styles.ts
@@ -6,14 +6,15 @@ import { theme } from 'theme';
 export const Container = styled(Flex)`
   width: 100vw;
   height: 100vh;
-  background-color: white;
+  background-color: ${theme.colors.background};
   .Center {
     margin: 0 auto;
     h3 {
+      font-family: coolvetica, sans-serif;
       text-align: center;
       margin-top: 18px;
       margin-bottom: 28px;
-      font-size: 28px;
+      font-size: 2.8vw;
       font-weight: bold;
     }
   }
