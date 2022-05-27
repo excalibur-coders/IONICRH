69c7e084e5163bbfad782cd29551a90231980706
ZeroPirata <gabriel0gsbn@gmail.com>
2022-05-27 12:34:33 -0300

    Rotas do cursos/trilhas

---
+++ ionic_rh_back/src/routes/curso.ts
@@ -0,0 +1,45 @@
+import {
+    createCurso, updateCurso, deleteCurso, readOneCurso, readManyCurso,
+    createTrilha, updateTrilha, deleteTrilha, readOneTrilha, readManyTrilha,
+    criarRelacao,
+    createRelationCursoTrilha,
+    consultaQueryCursoTrilhUser,
+    updateUserTrilha,
+    updateCursoTrilha,
+    removeUserTrilha,
+    removeCursoTrilha,
+    adicionarConteudo
+} from "controllers/cursos";
+import express from "express";
+import multerConfig from 'config/multercurso'
+import multer from 'multer';
+const router = express.Router();
+
+//Curso
+router.post('/criar-curso', createCurso)
+router.put('/atualizar-curso/:id', updateCurso)
+router.delete('/deletar-curso/:id', deleteCurso)
+router.get('/ver-curso/:id', readOneCurso)
+router.get('/ver-cursos', readManyCurso)
+//Trilha
+router.post('/criar-trilha', createTrilha)
+router.put('/atualizar-trilha/:id', updateTrilha)
+router.delete('/deletar-trilha/:id', deleteTrilha)
+router.get('/ver-trilha/:id', readOneTrilha)
+router.get('/ver-trilhas', readManyTrilha)
+//
+router.put('/alterar-curso-trilha/:trilhaTrilhaId/:cursoCursoId', updateCursoTrilha)
+router.delete('/remover-curso-trilha/:trilhaTrilhaId/:cursoCursoId', removeCursoTrilha)
+//
+router.put('/alterar-user-trilha/:trilhaTrilhaId/:userUserId', updateUserTrilha)
+router.delete('/remover-user-trilha/:trilhaTrilhaId/:userUserId', removeUserTrilha)
+//
+router.post('/vincular-user', criarRelacao)
+router.post('/adicionar-material/:docsDocsId',
+    multer(multerConfig)
+        .fields([{ name: 'file', maxCount: 3 }]),
+    adicionarConteudo)
+router.post('/vincular-curso', createRelationCursoTrilha)
+router.get('/ver-trilhas/:id', consultaQueryCursoTrilhUser)
+
+export default router; 
