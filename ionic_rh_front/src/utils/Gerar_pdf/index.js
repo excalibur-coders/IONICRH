0b993aca8afce6cfadf00fb8814f3a9c0d9a008e
Priscila  Silva <prialmeida1801@gmail.com>
2022-06-02 23:00:21 -0300

    inclusão dos campos e ajustes na interface pra gerar pdf

--- ionic_rh_front/src/utils/Gerar_pdf/index.js
+++ ionic_rh_front/src/utils/Gerar_pdf/index.js
@@ -1,14 +1,5 @@
-import pdfMake from 'pdfmake/build/pdfmake'
-import pdfFonts from 'pdfmake/build/vfs_fonts'
-
-// interface IUser {
-//   user_cpf?: string;
-//   user_rg: string;
-//   user_nascimento?: string;
-//   user_genero?: string;
-//   user_estado_civil?: string;
-//   user_nome?: string;
-// }
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 
 // function perfil_funcionarioPDF(usuario_perfil: IUser | undefined) {
 function perfil_funcionarioPDF(usuario_perfil) {
@@ -19,14 +10,14 @@ function perfil_funcionarioPDF(usuario_perfil) {
       text: 'User',
       fontSize: 15,
       bold: true,
-      margin: ['15, 20, 0, 45'] //left, top, right, bottom
-    }
+      margin: ['15, 20, 0, 45'], //left, top, right, bottom
+    },
   ];
 
   const contentEstilazado = [
     {
       alignment: 'center',
-      text: 'PPRA',
+      text: 'Ficha de Cadastro de Funcionários',
       style: 'header',
       fontSize: 23,
       bold: true,
@@ -36,8 +27,8 @@ function perfil_funcionarioPDF(usuario_perfil) {
       margin: [0, 0, 0, 10],
       layout: {
         fillColor: function (rowIndex, node, columnIndex) {
-          return (rowIndex % 2 === 0) ? '#ebebeb' : '#f5f5f5';
-        }
+          return rowIndex % 2 === 0 ? '#ebebeb' : '#f5f5f5';
+        },
       },
       table: {
         widths: ['100%'],
@@ -48,24 +39,39 @@ function perfil_funcionarioPDF(usuario_perfil) {
               text: `Nome: ${usuario_perfil?.user_nome}`,
               fontSize: 9,
               bold: true,
-            }
+            },
           ],
           [
             {
-              text: 'FUNÇÃO: DIRETOR DE ENSINO',
+              text: `Cargo: ${usuario_perfil?.cargo_id}`,
               fontSize: 9,
-              bold: true
-            }
+              bold: true,
+            },
+            {
+              text: `Head: ${usuario_perfil?.cargo_head}`,
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: `Nível: ${usuario_perfil?.cargo_nivel}`,
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: `Departamento: ${usuario_perfil?.dep_name}`,
+              fontSize: 9,
+              bold: true,
+            },
           ],
         ],
-      }
+      },
     },
     {
       style: 'tableExample',
       layout: {
         fillColor: function (rowIndex, node, columnIndex) {
-          return (rowIndex === 0) ? '#c2dec2' : null;
-        }
+          return rowIndex === 0 ? '#c2dec2' : null;
+        },
       },
       table: {
         widths: ['30%', '10%', '25%', '35%'],
@@ -74,187 +80,293 @@ function perfil_funcionarioPDF(usuario_perfil) {
         body: [
           [
             {
-              text: 'AGENTE: Não Identificados',
+              text: `Email: ${usuario_perfil?.user_email}`,
               colSpan: 3,
               bold: true,
-              fontSize: 9
-            },
-            {
-            },
-            {
-            },
-            {
-              text: 'GRUPO: Grupo 1 - Riscos Físicos',
               fontSize: 9,
-              bold: true
-            }
+            },
+            {
+              text: `Nascimento: ${usuario_perfil?.user_nascimento}`,
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: `RG: ${usuario_perfil?.user_rg}`,
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: `CPF: ${usuario_perfil?.user_cpf}`,
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: `Estado Civil: ${usuario_perfil?.user_estado_civil}`,
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: `Telefone: ${usuario_perfil?.tell_numero}`,
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: `Idioma: ${usuario_perfil?.idioma_falados}`,
+              fontSize: 9,
+              bold: true,
+            },
           ],
           [
             {
-              text: 'Limite de Tolerância:',
+              text: `Endereço: ${usuario_perfil?.endereco_rua}`,
+              colSpan: 3,
+              bold: true,
               fontSize: 9,
-              bold: true
             },
             {
-              text: 'Meio de Propagação:',
+              text: `Cidade: ${usuario_perfil?.endereco_cidade}`,
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: `Estado: ${usuario_perfil?.endereco_estado}`,
               colSpan: 3,
               fontSize: 9,
-              bold: true
+              bold: true,
             },
             {
+              text: `CEP: ${usuario_perfil?.endereco_cep}`,
+              colSpan: 3,
+              fontSize: 9,
+              bold: true,
             },
             {
-            }
+              text: `Numero: ${usuario_perfil?.endereco_numero}`,
+              colSpan: 3,
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: `Complemento: ${usuario_perfil?.endereco_compl}`,
+              colSpan: 3,
+              fontSize: 9,
+              bold: true,
+            },
           ],
           [
             {
               text: [
-                'Frequência: ',
+                `Matricula: ${usuario_perfil?.contrato_matricula}`,
                 {
                   text: 'Habitual',
-                  bold: false
-                }
+                  bold: false,
+                },
               ],
               fontSize: 9,
-              bold: true
+              bold: true,
             },
             {
               text: [
-                'Classificação do Efeito: ',
+                `Turno: ${usuario_perfil?.contrato_turno}`,
                 {
                   text: 'Leve',
-                  bold: false
-                }
+                  bold: false,
+                },
               ],
               colSpan: 3,
               fontSize: 9,
-              bold: true
+              bold: true,
             },
             {
+              text: [
+                `Base: ${usuario_perfil?.contrato_base}`,
+                {
+                  text: 'Habitual',
+                  bold: false,
+                },
+              ],
+              fontSize: 9,
+              bold: true,
             },
             {
-            }
+              text: [
+                `Head: ${usuario_perfil?.cargo_head}`,
+                {
+                  text: 'Leve',
+                  bold: false,
+                },
+              ],
+              colSpan: 3,
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: [
+                `Dominio: ${usuario_perfil?.contrato_dominio}`,
+                {
+                  text: 'Habitual',
+                  bold: false,
+                },
+              ],
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: [
+                `Nível: ${usuario_perfil?.argo_nivel}`,
+                {
+                  text: 'Leve',
+                  bold: false,
+                },
+              ],
+              colSpan: 3,
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: [
+                `Tipo de Contrato: ${usuario_perfil?.contrato_tipo}`,
+                {
+                  text: 'Habitual',
+                  bold: false,
+                },
+              ],
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: [
+                `Data de Admissao: ${usuario_perfil?.contrato_data_admissao}`,
+                {
+                  text: 'Leve',
+                  bold: false,
+                },
+              ],
+              colSpan: 3,
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: [
+                `Empresa Contratante: ${usuario_perfil?.contratante_nome}`,
+                {
+                  text: 'Habitual',
+                  bold: false,
+                },
+              ],
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: [
+                `Tempo de Formalização: ${usuario_perfil?.contrato_tempo_formalizacao}`,
+                {
+                  text: 'Leve',
+                  bold: false,
+                },
+              ],
+              colSpan: 3,
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: [
+                `Tempo de Casa: ${usuario_perfil?.contrato_tempo_de_casa}`,
+                {
+                  text: 'Habitual',
+                  bold: false,
+                },
+              ],
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: [
+                `Faixa Salarial: ${usuario_perfil?.contrato_faixa_salarial}`,
+                {
+                  text: 'Leve',
+                  bold: false,
+                },
+              ],
+              colSpan: 3,
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: [
+                `Termo de PJ: ${usuario_perfil?.contrato_termos}`,
+                {
+                  text: 'Habitual',
+                  bold: false,
+                },
+              ],
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: [
+                `Data de Desligamento: ${usuario_perfil?.contrato_data_desligamento}`,
+                {
+                  text: 'Leve',
+                  bold: false,
+                },
+              ],
+              colSpan: 3,
+              fontSize: 9,
+              bold: true,
+            },
+            {
+              text: [
+                `Distrato: ${usuario_perfil?.contrato_distrato}`,
+                {
+                  text: 'Habitual',
+                  bold: false,
+                },
+              ],
+              fontSize: 9,
+              bold: true,
+            },
           ],
           [
             {
-              text: 'Tempo de Exposição:',
+              text: `Plano de Saúde: ${usuario_perfil?.contrato_plano_saude}`,
               colSpan: 2,
               fontSize: 9,
-              bold: true
+              bold: true,
             },
+            {},
             {
-            },
-            {
-              text: 'Medição:',
+              text: `Auxílio Creche: ${usuario_perfil?.contrato_auxilio_creche}`,
               colSpan: 2,
               fontSize: 9,
-              bold: true
+              bold: true,
             },
             {
-            }
-          ],
-          [
-            {
-              text: 'Fonte Geradora:',
+              text: `Vale Transpote: ${usuario_perfil?.contrato_vale_transporte}`,
               border: [true, true, false, false],
               colSpan: 2,
               fontSize: 9,
-              bold: true
+              bold: true,
             },
             {
-            },
-            {
-              text: 'Téc. Utilizada:',
-              border: [false, true, true, false],
+              text: `Vale Alimentação: ${usuario_perfil?.contrato_vale_alimentacao}`,
+              border: [true, true, false, false],
               colSpan: 2,
               fontSize: 9,
-              bold: true
+              bold: true,
             },
             {
-            }
-          ],
-          [
-            {
-              text: 'Conclusão:',
-              border: [true, false, true, true],
-              colSpan: 4,
+              text: `Vale Refeição: ${usuario_perfil?.contrato_vale_refeicao}`,
+              border: [true, true, false, false],
+              colSpan: 2,
               fontSize: 9,
-              bold: true
+              bold: true,
             },
-            {
-            },
-            {
-            },
-            {
-            }
           ],
-          [
-            {
-              text: 'EPIs/EPCs:',
-              border: [true, true, false, true],
-              colSpan: 3,
-              fontSize: 9,
-              bold: true
-            },
-            {
-            },
-            {
-            },
-            {
-              text: 'CAs:',
-              border: [false, true, true, true],
-              fontSize: 9,
-              bold: true
-            }
-          ],
-        ]
-      }
-    }
-  ]
-
-  const dados = [
-    // usuario_perfil.user_cpf,
-    // usuario_perfil.user_rg
-    // {text: usuario_perfil.user_cpf, fontSize: 9, margin: [0, 2, 0, 2]},
-    // {text: usuario_perfil.user_rg, fontSize: 9, margin: [0, 2, 0, 2]},
-  ]
-  // usuario_perfil.map((usuario) => {
-  //   return [
-  //     {text: usuario.id, fontSize: 9, margin: [0, 2, 0, 2]},
-  //     {text: usuario.nome, fontSize: 9, margin: [0, 2, 0, 2]},
-  //     {text: usuario.email, fontSize: 9, margin: [0, 2, 0, 2]},
-  //     {text: usuario.telefone, fontSize: 9, margin: [0, 2, 0, 2]},
-  //   ]
-  // });
-
-  // const details  = [
-  //   {
-  //     table: {
-  //       headerRows: 1,
-  //       widths: ['*', '*', '*', '*'], //definição das colunas, o * ajusta automatico
-  //       body: [
-  //         [
-  //           {text: 'RG', style: 'tableHeader', fontSize: 10},
-  //           {text: 'CPF', style: 'tableHeader', fontSize: 10},
-  //         ],
-  //         ...dados
-
-  //       ]
-  //     },
-  //     layout: 'lightHorizontalLines'
-  //   }
-  // ]
-
-  // function rodape(currentPage, pageCount){
-  //   return [
-  //     {
-  //       text: currentPage + ' / ' + pageCount,
-  //       alignment: 'right',
-  //       fontSize: 9,
-  //       margin: ['0, 10, 20, 0'] //left, top, right, bottom
-  //     }
-  //   ]
-  // }
+        ],
+      },
+    },
+  ];
 
   const docDefinitions = {
     // pagesSize: 'A4',
@@ -263,8 +375,8 @@ function perfil_funcionarioPDF(usuario_perfil) {
     // header: [reportTitle],
     content: contentEstilazado,
     // footer: [rodape]
-  }
-  console.log('opa')
+  };
+
   pdfMake.createPdf(docDefinitions).download();
 }
 
